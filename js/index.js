10e7c6fc6f91d591c0080e47852ae6c7501a973d
Dulce Garcia <dulcega75@gmail.com>
2026-06-11 00:23:10 -0500

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -68,3 +68,25 @@ messageForm.addEventListener("submit", function (event) {
 
   messageForm.reset();
 });
+
+fetch("https://api.github.com/users/DulceeGP/repos")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (repositories) {
+    console.log(repositories);
+
+    const projectSection = document.querySelector("#projects");
+    const projectList = projectSection.querySelector("ul");
+
+    for (let i = 0; i < repositories.length; i++) {
+      const project = document.createElement("li");
+
+      project.innerText = repositories[i].name;
+
+      projectList.appendChild(project);
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
